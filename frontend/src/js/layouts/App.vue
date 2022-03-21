100ae133a95437807d5d54e1b9b4886ea9b4dfcc
Selvin Ortiz <selvin@selvin.co>
2022-03-21 15:02:59 -0500

    Update App layout to align with the Welcome/Start rename

--- frontend/src/js/layouts/App.vue
+++ frontend/src/js/layouts/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="h-full flex flex-col" :class="getBackgroundClasses()">
-    <!-- :style="{ backgroundImage: isOnWelcomeScreen ? 'url(assets/images/varly-background.png)' : '' }" -->
+    <!-- :style="{ backgroundImage: isOnStartScreen ? 'url(assets/images/varly-background.png)' : '' }" -->
     <header data-wails-drag>
-      <nav class="flex items-center justify-end px-2 py-4">
+      <nav class="flex items-center justify-end px-2 py-3">
         <ul class="flex items-center">
           <!-- <li>
-            <router-link :to="{ name: 'welcome' }" class="px-2 py-4 opacity-75 font-semibold">Home</router-link>
+            <router-link :to="{ name: 'start' }" class="px-2 py-4 opacity-75 font-semibold">Home</router-link>
           </li>-->
           <!-- <li>
             <button @click="open('https://twitter.com/varlyapp')" class="p-2 opacity-50">Support</button>
@@ -15,9 +15,9 @@
     </header>
     <router-view class="flex-1 overflow-auto scrollbar-none"></router-view>
     <footer class="text-center px-8 py-4">
-      <slot name="footer">
+      <!-- <slot name="footer">
         <p class="text-sm opacity-75">&copy;2022 Varly v1.0.0</p>
-      </slot>
+      </slot> -->
     </footer>
   </div>
 </template>
@@ -27,19 +27,19 @@ import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
-const isOnWelcomeScreen = ref(false)
+const isOnStartScreen = ref(false)
 
-function setIsOnWelcomeScreen(to) {
-  isOnWelcomeScreen.value = to.name === 'welcome'
+function setIsOnStartScreen(to) {
+  isOnStartScreen.value = to.name === 'start'
 }
 
 function getBackgroundClasses() {
-  if (isOnWelcomeScreen.value) {
+  if (isOnStartScreen.value) {
     return 'bg-gradient-to-b from-slate-100 to-slate-200 dark:from-purple-600 dark:to-purple-800'
   }
 
   return 'bg-gradient-to-b from-slate-100 to-slate-200 dark:from-slate-800 dark:to-slate-900'
 }
 
-router.beforeResolve(setIsOnWelcomeScreen)
+router.beforeResolve(setIsOnStartScreen)
 </script>
