255cb0b707fd11cf3fdef254e765362b804a975a
Selvin Ortiz <selvin@selvin.co>
2022-03-21 14:59:36 -0500

    Add Base64 vue component and update Start screen

---
+++ frontend/src/js/components/Base64Image.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref, onBeforeMount } from 'vue'
+
+const image = ref('')
+
+const props = defineProps(['path'])
+
+onBeforeMount(async () => {
+    image.value = await window.go.main.App.EncodeImage(props.path)
+    // let old = 0
+    // let path = props.path
+    // for (let index = 0; index < 10; index++) {
+    //     if (index === 0) {
+    //     } else {
+    //         path = path.replace(`${old}`, `${index}`)
+
+    //         image.value = await window.go.main.App.EncodeImage(path)
+
+    //         old = index - 1
+
+    //         console.log({ old, index })
+    //         console.log({ path, props })
+    //     }
+
+    //     await new Promise(resolve => setTimeout(resolve, 2000))
+    // }
+})
+</script>
+
+<template>
+    <img :src="image" alt="Base 64 Encoded Image" height="128" />
+</template>
---
+++ frontend/src/js/pages/Start.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="h-full flex flex-col items-center justify-center">
+    <section class="flex items-center justify-center">
+      <Icon
+        class="mx-8"
+        name="new"
+        text="Start NFT Project"
+        @click="() => router.push({ name: 'art.collection.new' })"
+      />
+      <Icon
+        class="mx-8"
+        name="import"
+        text="Import Document"
+        @click="() => open('https://twitter.com/varlyapp')"
+      />
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import Base64Image from '@root/components/Base64Image.vue'
+
+import Icon from '@components/Icon.vue'
+
+const router = useRouter()
+
+function tapped() {
+  window.console.log('Hello')
+}
+
+function open(url) {
+  window.runtime.BrowserOpenURL(url)
+}
+</script>
