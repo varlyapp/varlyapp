15bf6b2f7465b4c02058026adedaae6b06803edb
Selvin Ortiz <selvin@selvin.co>
2022-03-25 21:35:35 -0500

    Improve light/dark theme typographic colors

--- frontend/src/js/pages/collections/StartCollection.vue
+++ frontend/src/js/pages/collections/StartCollection.vue
@@ -6,22 +6,22 @@ const varly = inject<Varly>('varly')!
 </script>
 
 <template>
-    <div class="h-ful container mx-auto p-8">
-        <form class="h-full space-y-8 divide-y divide-slate-200 flex flex-col justify-between">
-            <div class="space-y-8 divide-y divide-slate-200">
+    <div class="h-full container mx-auto p-8">
+        <form class="h-full space-y-8 divide-y divide-slate-100 dark:divide-slate-800 flex flex-col justify-between">
+            <div>
                 <div>
                     <div>
-                        <h1 class="text-lg leading-6 font-medium text-slate-900">New Collection</h1>
+                        <h1 class="text-lg leading-6 font-medium">New Collection</h1>
                         <p
-                            class="mt-1 text-sm text-slate-500"
-                        >Please provide details about your new collection.</p>
+                            class="mt-1 text-sm opacity-75"
+                        >Please provide details about your new NFT collection</p>
                     </div>
 
                     <div class="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
                         <div class="sm:col-span-6">
                             <label
                                 for="collection-name"
-                                class="block text-sm font-medium text-slate-700"
+                                class="block text-sm opacity-75"
                             >Collection Name</label>
                             <div class="mt-1">
                                 <input
@@ -29,7 +29,7 @@ const varly = inject<Varly>('varly')!
                                     name="collection-name"
                                     id="collection-name"
                                     autocomplete="none"
-                                    class="shadow-sm focus:ring-purple-500 focus:border-purple-500 block w-full sm:text-sm border-slate-300 rounded-sm"
+                                    class="text-slate-700 dark:bg-slate-100 shadow-sm focus:ring-purple-500 focus:border-purple-500 block w-full sm:text-sm border-slate-300 rounded-sm"
                                 />
                             </div>
                         </div>
@@ -37,14 +37,14 @@ const varly = inject<Varly>('varly')!
                         <div class="sm:col-span-6">
                             <label
                                 for="about"
-                                class="block text-sm font-medium text-slate-700"
+                                class="block text-sm opacity-75"
                             >Collection Description</label>
                             <div class="mt-1">
                                 <textarea
                                     id="about"
                                     name="about"
-                                    rows="3"
-                                    class="shadow-sm focus:ring-purple-500 focus:border-purple-500 block w-full sm:text-sm border border-slate-300 rounded-sm"
+                                    rows="4"
+                                    class="text-slate-700 dark:bg-slate-100 shadow-sm focus:ring-purple-500 focus:border-purple-500 block w-full sm:text-sm border border-slate-300 rounded-sm"
                                 />
                             </div>
                         </div>
