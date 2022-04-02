82c5da313a5d24b5a8a00e3a3ef4b67acb91c5ae
Selvin Ortiz <selvin@selvin.co>
2022-04-01 23:32:05 -0500

    Remove unused store vars and clean up usage in App.vue

--- frontend/src/js/layouts/App.vue
+++ frontend/src/js/layouts/App.vue
@@ -3,25 +3,17 @@
 </template>
 
 <script setup lang="ts">
-import { useI18n } from 'vue-i18n'
-import { ref, onBeforeUnmount, onBeforeMount } from 'vue'
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
-import { useStore } from '@root/store'
 
-const intl = useI18n({ useScope: 'global' })
 const router = useRouter()
-const store = useStore()
-
 const isOnStartScreen = ref(false)
-onBeforeUnmount(() => store.actions = [])
 
 function setIsOnStartScreen(to) {
   isOnStartScreen.value = to.name === 'start'
 }
 
 router.beforeResolve((to) => {
-  store.actions = []
-
   setIsOnStartScreen(to)
 })
 </script>
--- frontend/src/js/store.ts
+++ frontend/src/js/store.ts
@@ -1,34 +1,26 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
-interface Action {
-  type: string
-  title: string
-  onClick: Function
-}
-
 const useStore = defineStore('app', () => {
   const locale = ref<string>('en')
   const isDark = ref<boolean>(false)
-  const isLoading = ref<boolean>(false)
-  const actions = ref<Array<Action>>([])
 
   function setLocale(_locale: string) {
     locale.value = _locale
   }
 
-  return { locale, isDark, isLoading, actions, setLocale }
+  return { locale, isDark, setLocale }
 }, { persist: true })
 
 const useCollectionStore = defineStore('collection', () => {
-  const traits = ref<Object[]>([])
-  const layers = ref<Object>({})
-  const directory = ref<string>('') // where the layers were loaded from
-  const name = ref<string>('')
+  const traits      = ref<Object[]>([])
+  const layers      = ref<Object>({})
+  const directory   = ref<string>('')    // where the layers were loaded from
+  const name        = ref<string>('')
   const description = ref<string>('')
-  const size = ref<number>(0)
-  const width = ref<number>(0)
-  const height = ref<number>(0)
+  const size        = ref<number>(0)
+  const width       = ref<number>(0)
+  const height      = ref<number>(0)
 
   function reset(this: any) {
     this.traits = []
