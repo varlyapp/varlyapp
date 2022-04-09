01e4ca9edc490942358b48c21d97282d38ec5e2e
Selvin Ortiz <selvin@selvin.co>
2022-04-09 17:53:32 -0500

    Declare layer, trait, and collection types

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,4 +1,4 @@
-enum LoggerType {
+export enum LoggerType {
     Debug = 'Debug',
     Error = 'Error',
     Fatal = 'Fatal',
@@ -7,4 +7,30 @@ enum LoggerType {
     Warning = 'Warning',
 }
 
-export { LoggerType }
+type Layer = {
+    name: string
+    path: string
+    width: number
+    height: number
+    weight: number
+    type: string
+}
+
+type Trait = {
+    name: string;
+    collapsed: boolean;
+}
+
+type Collection = {
+    name: string
+    description: string
+    sourceDirectory: string
+    outputDirectory: string
+    traits: Trait[] | Object[]
+    layers: {[key: string]: Layer[]} | Object
+    width: number
+    height: number
+    size: number
+}
+
+export type { Layer, Trait, Collection }
