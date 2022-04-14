7d5c85768465d0dbb565c465e0accf44968b1995
Selvin Ortiz <selvin@selvin.co>
2022-04-14 13:33:24 -0500

    Add floating button bar component

---
+++ frontend/src/components/FloatingButtonBar.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="w-2/3 absolute z-auto bottom-8 right-8">
+        <div class="flex items-center justify-end gap-4">
+            <slot></slot>
+        </div>
+    </div>
+</template>
