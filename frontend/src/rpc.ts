9edf01a2b792c99cef1a652166c460e8c58fcab7
Selvin Ortiz <selvin@selvin.co>
2022-04-25 01:25:32 -0500

    Add the ability to set a page title with prefix in RCP

--- frontend/src/rpc.ts
+++ frontend/src/rpc.ts
@@ -1,4 +1,10 @@
 const app = window.go.main.App
-const rpc = { app, ...window.go.services }
+const rpc = { app, ...window.go.services, setPageTitle }
+
+async function setPageTitle(title: string) {
+    const prefix = await app.Title()
+
+    window.runtime.WindowSetTitle(`${prefix} — ${title}`)
+}
 
 export default rpc
