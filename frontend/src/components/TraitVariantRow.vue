020fd96d23c95e877d2fdeb513ca03d7de24ae14
Selvin Ortiz <selvin@selvin.co>
2022-05-11 00:42:38 -0500

    Update trait variant row component to use asset import reference in style tag rather than inline

--- frontend/src/components/TraitVariantRow.vue
+++ frontend/src/components/TraitVariantRow.vue
@@ -11,16 +11,22 @@ function getRowClasses(index) {
 }
 </script>
 
+<style>
+.opacity-background {
+    background: url("@/assets/opacity-pattern.png") repeat left top;
+}
+</style>
+
 <template>
     <div :key="i" v-for="(variant, i) in props.variants"
         class="min-w-full flex items-center justify-between border-t border-slate-900 dark:border-slate-50 border-opacity-20 dark:border-opacity-20"
         :class="getRowClasses(i)">
         <div class="w-1/">
-            <img class="w-16 p-2" :src="variant.path" alt="" />
+            <img class="w-16 m-2 opacity-background" :src="variant.path" alt="" />
         </div>
 
         <div
-            class="w-4/6 border-r border-l border-dotted text-left whitespace-nowrap px-4 text-sm font-medium sm:px-6 lg:px-8"
+            class="w-4/6 text-left whitespace-nowrap px-4 text-sm font-medium sm:px-6 lg:px-8"
             v-text="variant.name" />
         <div class="w-1/6 pr-2">
             <input
